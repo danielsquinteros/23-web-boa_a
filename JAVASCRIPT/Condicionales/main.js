0b7bc2d4b34b6731b1a525a04db4f218dac0a427
Daniel Quinteros <danielsquinteros@gmail.com>
2023-02-06 20:53:24 -0300

    javascript

---
+++ JAVASCRIPT/Condicionales/main.js
@@ -0,0 +1,21 @@
+//NOSOTROS COMO DESARROLLADORES
+// ver como va nuestro código
+
+// Impresión normal
+console.log('Hola mundo')
+
+//Impresión de un error
+console.error('Hola mundo')
+
+//Impresión de una alerta
+console.warn('Hola mundo')
+
+
+// PARA EL USUARIO
+// ENVIAR MENSAJE AL USUARIO
+alert('HOLA BIENVENIDO')
+
+// INGRESAR DATOS
+var nota1 = prompt('Felipe, ingresa tu primera nota')
+console.log(nota1);
+console.log(typeof nota1);
